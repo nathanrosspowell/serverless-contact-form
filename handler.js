093278338dd555746fb0c948a4fc17f9d7993b94
Nathan Ross Powell <nathanrosspowell@gmail.com>
2019-05-04 15:22:27 -0400

    Basic AWS email

--- handler.js
+++ handler.js
@@ -1,28 +1,72 @@
 'use strict';
 
-var qs = require('qs')
+const aws = require('aws-sdk');
+const qs = require('qs');
+const ses = new aws.SES();
+const region = "<<{{MyRegionName}}>>";
+const secretName = process.env.secret_manager_token;
+const myEmail = "nathanrosspowell@gmail.com"; //process.env.secrets.EMAIL
+const myDomain = "*"; //process.env.secrets.DOMAIN
 
-module.exports.contact = (event, context, callback) => {
-  console.log('Received event:', JSON.stringify(event, null, 2));
-
-  var data = qs.parse(event.body);
-
-  var name = data.name;
-  var subject = data.subject;
-  var email = data.email;
-  var message = data.message;
-
-  const response = {
-    statusCode: 200,
+function generateResponse (code, payload) {
+  return {
+    statusCode: code,
     headers: {
-      "x-custom-header" : "Contact Form Reply"
+      'Access-Control-Allow-Origin': myDomain,
+      'Access-Control-Allow-Headers': 'x-requested-with',
+      'Access-Control-Allow-Credentials': true
     },
-    body: JSON.stringify({
-      status: 'OK',
-      name: name,
-      subject: subject,
-      email: email, 
-      message: message}),
-  };
-  callback(null, response);
-};
+    body: JSON.stringify(payload)
+  }
+}
+
+function generateError (code, err) {
+  console.log(err)
+  return {
+    statusCode: code,
+    headers: {
+      'Access-Control-Allow-Origin': myDomain,
+      'Access-Control-Allow-Headers': 'x-requested-with',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify(err.message)
+  }
+}
+
+function generateEmailParams (body) {
+  const { name, subject, email, message } = qs.parse(body);
+  console.log(name, subject, email, message);
+  if (!(name && subject && email && message)) {
+    throw new Error('Missing parameters! Make sure to add parameters, \'name\', \'subject\', \'email\' & \'message\'.')
+  }
+
+  console.log('myEmail:', myEmail, ", myDomain:", myDomain);
+  console.log(process.env);
+  return {
+    Source: myEmail,
+    Destination: { ToAddresses: [myEmail] },
+    ReplyToAddresses: [email],
+    Message: {
+      Body: {
+        Text: {
+          Charset: 'UTF-8',
+          Data: `From: ${name}\nEmail: ${email}\nMessage: ${message}`
+        }
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: `${name} has send you a message.`
+      }
+    }
+  }
+}
+
+module.exports.contact = async (event) => {
+  try {
+    const emailParams = generateEmailParams(event.body)
+    const data = await ses.sendEmail(emailParams).promise()
+    return generateResponse(200, data)
+  } catch (err) {
+    return generateError(500, err)
+  }
+}
